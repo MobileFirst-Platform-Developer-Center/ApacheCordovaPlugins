e4ed6a674f5debd25a7c1a1af98e3a3c5221ec92
Lior Burg <liorbu@il.ibm.com>
2015-07-29 16:06:16 +0300

    Upgrade ApacheCordovaPlugins sample

--- apps/CordovaPlugin/iphone/native/Classes/CordovaPlugin.m
+++ apps/CordovaPlugin/iphone/native/Classes/CordovaPlugin.m
@@ -10,7 +10,7 @@
 */
 
 #import "CordovaPlugin.h"
-#import "WLWebFrameworkInitResult.h"
+#import <IBMMobileFirstPlatformFoundationHybrid/IBMMobileFirstPlatformFoundationHybrid.h>
 #import "Cordova/CDVViewController.h"
 
 @implementation MyAppDelegate
